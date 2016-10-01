52b3d11244d071d7f4ab2a9a8c1de64db4e07bcf
yishibashi <yishibashi101@gmail.com>
2016-10-01 19:08:35 +0900

    ( ﾟけっこう(´・ω・｀)むずかし

--- CIS194_2013/Week02/code/LogAnalysis.hs
+++ CIS194_2013/Week02/code/LogAnalysis.hs
@@ -18,3 +18,35 @@ parse s = map parseMessage $ lines s
 
 
 {-- Exercise 2 --}
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) msgT = msgT
+insert msg Leaf = Node Leaf msg Leaf
+insert msg@(LogMessage _ ts _) (Node left msg'@(LogMessage _ ts' _) right)
+  |ts <= ts' = Node (insert msg left) msg' right
+  |otherwise = Node left msg' (insert msg right)
+
+
+{-- Exercise 3 --}
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+
+{-- Exercise 4--}
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) =  inOrder left ++ [msg] ++ inOrder right
+
+
+{-- Exercise 5 --}
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logs = map getMsg $ filter sev_50ovr msgs
+  where msgs = inOrder $ build logs
+        -- vvvvv functions vvvvv
+        sev_50ovr (LogMessage (Error sev) _ _) = sev >= 50
+        sev_50ovr _ = False
+        getMsg (LogMessage _ _ m) = m
+        getMsg (Unknown _) = ""
